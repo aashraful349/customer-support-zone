e495916b172e56636002b90a6ec6e1ecd0a64393
aashraful349 <aashraful349@gmail.com>
2026-03-11 15:27:48 +0600

    worked on the navbar just need to add functionality

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl font-bold">CS — Ticket System</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li><a>FAQ</a></li>
+                        <li><a>Changelog</a></li>
+                        <li><a>Blog</a></li>
+                        <li><a>Download</a></li>
+                        <li><a>Contact</a></li>
+                        <li><button className='text-white font-semibold bg-gradient-to-r from-[#632EE3] to-[#9F62F2]'>New Ticket</button></li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
